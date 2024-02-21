b739f19459e7e0397394697d4114ffce0508ca53
SeanOBrien2702 <seanobrien2702@gmail.com>
2024-02-21 00:20:55 -0500

    Most recent dialogue options shown in history panel

--- Assets/Scripts/Core/Constants.cs
+++ Assets/Scripts/Core/Constants.cs
@@ -3,5 +3,7 @@ public static class K
     public static string GameScene = "GameScene";
     public static string MenuScene = "MainMenu";
 
+    public static string UILayer = "UI";
+
     public static int HexCodeLength = 6;
 }
---
+++ Assets/Scripts/HistoryController.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HistoryController : MonoBehaviour
+{
+    Queue<TextMeshProUGUI> historyList = new Queue<TextMeshProUGUI>();
+    [SerializeField] Transform content;
+    [SerializeField] TextMeshProUGUI textPrefab;
+    [SerializeField] int maxHistoryCount;
+    bool isShown = false;
+    CanvasGroup canvas;
+    Image image;
+
+    public bool IsShown { get => isShown; set => isShown = value; }
+
+    void Start()
+    {
+        canvas = GetComponent<CanvasGroup>();
+        image = GetComponent<Image>();
+        canvas.alpha = isShown ? 1 : 0;
+        DialogueController.OnNewDialogue += DialogueController_OnNewDialogue;
+    }
+
+    private void OnDestroy()
+    {
+        DialogueController.OnNewDialogue -= DialogueController_OnNewDialogue;
+    }
+
+    private void DialogueController_OnNewDialogue(Dialogue dialogue)
+    {
+        TextMeshProUGUI newText = Instantiate(textPrefab, content);
+        newText.text = dialogue.DialogueText;
+        historyList.Enqueue(newText);
+        if(historyList.Count > maxHistoryCount)
+        {
+            Destroy(historyList.Dequeue().gameObject);
+        }
+    }
+
+    public void ToggleHistory()
+    {
+        isShown = !isShown;
+        image.raycastTarget = isShown;
+        canvas.alpha = isShown ? 1 : 0;
+    }
+}
